5e43954c2faa50ae629443f7cef059c3da93a98f
Khalid Hassan <khalidhsoliman@gmail.com>
2018-01-25 10:39:25 +0200

    AddHazardDamageScript

---
+++ Assets/HazardDamage.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HazardDamage : MonoBehaviour {
+
+    public int damageAmount = 5;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.tag == "Player")
+        {
+            CharacterController2D player = collision.gameObject.GetComponent<CharacterController2D>();
+
+            player.ApplyDamage(damageAmount); 
+        }
+    }
+}
